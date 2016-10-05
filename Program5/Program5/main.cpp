a177fdf9809211de25bda59d6d9e686304593eac
AlexAndrei98 <alexandrei1998@hotmail.it>
2016-10-04 20:22:56 -0500

    Add files via upload

---
+++ Program5/Program5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Program5
+//
+//  Created by Alexandru on 10/3/16.
+//  Copyright © 2016 Alexandru. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
